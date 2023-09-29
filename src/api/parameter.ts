a728b082d4374d6bdab6fdef0264671ac67c8d22
MaartenBoonHoGent <100087910+MaartenBoonHoGent@users.noreply.github.com>
2023-09-29 13:59:55 +0200

    parameter integration complete

--- src/api/parameter.ts
+++ src/api/parameter.ts
@@ -1,9 +1,13 @@
 import { apiRequest } from "@/api/request";
+import { setLocalStorage } from "@/api/localStorage";
 
 const BASE = `${import.meta.env.VITE_APP_API_URL}/parameter`;
 
 const getParameters = async () => {
-  return await apiRequest("GET", BASE, {}, true);
+  let parameters = await apiRequest("GET", BASE, {}, true);
+  // set the parameters in local storage
+  setLocalStorage("parameters", JSON.stringify(parameters.data));
+  return parameters;
 };
 
 export { getParameters };
--- src/api/summary.ts
+++ src/api/summary.ts
@@ -1,17 +1,72 @@
 import { apiRequest, apiPostFile } from "@/api/request";
+import { getLocalStorage } from "./localStorage";
 
 const BASE = `${import.meta.env.VITE_APP_API_URL}/summary`;
 const FILE_TYPES = ["md", "pdf", "docx", "txt"];
 const MAX_FILE_SIZE = 251658240; // 30MB
-const MIME_TYPES = [
-  "text/markdown",
-  "application/pdf",
-  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  "text/plain",
-];
 
 const getSummariesForHome = async () => {
   return await apiRequest("GET", `${BASE}/home`, {}, true);
+};
+
+const checkParameters = (parameters: any[]) => {
+  // Make sure that there's at least one parameter
+  if (parameters.length == 0)
+    return { status: 400, message: "No parameters were included", data: {} };
+
+  // Get the parameters from local storage
+
+  let params = getLocalStorage("parameters");
+  if (!params)
+    return { status: 400, message: "Parameters not found", data: {} };
+
+  let parsedParams = JSON.parse(params);
+
+  // Get the distinct parameter classes based on class id
+  let classes = parsedParams.map((p: any) => p.class);
+  // Use reduce and Map to extract distinct classes based on id
+  classes = Array.from(
+    classes
+      .reduce((map: any, item: any) => map.set(item.id, item), new Map())
+      .values()
+  );
+
+  // Add an array to each class, which will contain the parameter ids
+  classes = classes.map((c: any) => {
+    return { class: c, param: [] };
+  });
+  // Add the parameters to their respective classes
+  for (let i = 0; i < parsedParams.length; i++) {
+    let j = parsedParams[i];
+    let index = classes.findIndex((c: any) => c.class.id === j.class.id);
+    classes[index].param.push(j);
+  }
+
+  for (let i = 0; i < classes.length; i++) {
+    let c = classes[i];
+    let parametersIncluded = 0;
+    for (let j = 0; j < c.param.length; j++) {
+      let p = c.param[j];
+      if (parameters.find((param: any) => param.id === p.id))
+        parametersIncluded++;
+    }
+    // Check if the number of parameters included in the class is allowed
+    if (parametersIncluded == 0 && c.class.allow_empty == 0) {
+      return {
+        status: 400,
+        message: `No parameters were included in '${c.class.name}'`,
+        data: {},
+      };
+    }
+    if (parametersIncluded > 1 && c.class["selection_type"] == 0) {
+      return {
+        status: 400,
+        message: `More than one parameter was included in '${c.class.name}', but only one is allowed`,
+        data: {},
+      };
+    }
+  }
+  return { status: 200 };
 };
 
 const newSummary = async (
@@ -29,7 +84,6 @@ const newSummary = async (
   file: File | null
 ) => {
   // Returns: { status: number, data: any, message: any}
-  console.log(file, file?.size, file?.type, file?.name);
   // Check the file size, type, extension, if it even exists
   if (!file) return { status: 400, message: "No file was uploaded", data: {} };
   if (file.size > MAX_FILE_SIZE)
@@ -41,20 +95,8 @@ const newSummary = async (
       data: {},
     };
 
-  // Check the parameters
-  // Find the distinct parameterClasses
-  const parameterClasses = parameters.map((p: any) => p.class.id);
-  const distinctParameterClasses = [...new Set(parameterClasses)];
-
-  // Check if the parameterClasses are valid
-  if (distinctParameterClasses.length !== parameterClassesCount)
-    return {
-      status: 400,
-      message: "Not every parameter has been filled in.",
-      data: {},
-    };
-
-  // Change the parameters to be an array of ids
+  const check = checkParameters(parameters);
+  if (check.status !== 200) return check;
 
   return await apiPostFile(
     `${BASE}/new`,
